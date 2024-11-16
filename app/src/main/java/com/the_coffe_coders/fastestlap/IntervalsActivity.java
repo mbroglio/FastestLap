f736e16b76a39ded4892087b6859ed00c6957f98
Matty-Tuber <m.broglio3@campus.unimib.it>
2024-11-16 17:24:03 +0100

    fixed merge conflicts, updated missing dependencies

--- app/src/main/java/com/the_coffe_coders/fastestlap/IntervalsActivity.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/IntervalsActivity.java
@@ -10,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
@@ -18,6 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.threeten.bp.ZoneId;
 import org.threeten.bp.ZonedDateTime;
 
 import java.util.HashMap;
@@ -60,6 +62,36 @@ public class IntervalsActivity extends AppCompatActivity {
         testIntervals();
     }
 
+    private void getIntervals() {
+        ZonedDateTime nowUtc = ZonedDateTime.now(ZoneId.of("UTC"));
+        ZonedDateTime prevUtc = nowUtc.minusMinutes(1);
+
+        // Define a runnable that will repeatedly call the API
+        Runnable intervalRunnable = new Runnable() {
+            ZonedDateTime currentNowUtc = nowUtc;
+            ZonedDateTime currentPrevUtc = prevUtc;
+
+            @Override
+            public void run() {
+                Log.i(TAG, "Now --> " + currentNowUtc);
+                Log.i(TAG, "Prev --> " + currentPrevUtc);
+
+                requestIntervals("latest", currentNowUtc, currentPrevUtc);
+
+                int secondsDelay = 5;
+                // Update the timestamps
+                currentPrevUtc = currentNowUtc;
+                currentNowUtc = currentPrevUtc.plusSeconds(secondsDelay);
+
+                // Post the runnable again with a delay of 5 seconds
+                handler.postDelayed(this, secondsDelay * 1000);
+            }
+        };
+
+        // Start the initial runnable
+        handler.post(intervalRunnable);
+    }
+
     private void testIntervals() {
         ZonedDateTime nowUtc = ZonedDateTime.parse("2024-07-07T14:03:15+00:00");
         ZonedDateTime prevUtc = nowUtc.minusMinutes(1);
@@ -97,7 +129,7 @@ public class IntervalsActivity extends AppCompatActivity {
 
         call.enqueue(new Callback<ResponseBody>() {
             @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
                 Log.i(TAG, "onResponse: " + response.code() + " " + response.message());
                 if (response.isSuccessful() && response.body() != null) {
                     try {
@@ -126,7 +158,7 @@ public class IntervalsActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
                 Log.e(TAG, "Request failed: " + t.getMessage(), t);
             }
         });
@@ -154,12 +186,12 @@ public class IntervalsActivity extends AppCompatActivity {
                 intervalValue = interval.getDouble("interval");
                 driver.updateInterval(intervalValue);
 
-                Log.e(TAG, "Missing gap_to_leader in interval: " + interval.toString());
+                Log.e(TAG, "Missing gap_to_leader in interval: " + interval);
             } else if (interval.isNull("interval")) {
                 gapToLeader = interval.getDouble("gap_to_leader");
                 driver.updateGapToLeader(gapToLeader);
 
-                Log.e(TAG, "Missing interval in interval: " + interval.toString());
+                Log.e(TAG, "Missing interval in interval: " + interval);
             } else {
                 gapToLeader = interval.getDouble("gap_to_leader");
                 intervalValue = interval.getDouble("interval");
@@ -207,162 +239,162 @@ public class IntervalsActivity extends AppCompatActivity {
             case 1:
                 // Max Verstappen
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.redbull_f1));
-                driverName.setText("Max Verstappen");
+                driverName.setText(R.string.max_verstappen);
                 driverNumber.setImageResource(R.drawable.max_verstappen_number);
                 break;
 
             case 4:
                 // Lando Norris
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.mclaren_f1));
-                driverName.setText("Lando Norris");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.lando_norris);
+                driverNumber.setImageResource(R.drawable.lando_norris_number);
                 break;
 
             case 16:
                 // Charles Leclerc
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.ferrari_f1));
-                driverName.setText("Charles Leclerc");
+                driverName.setText(R.string.charles_leclerc);
                 driverNumber.setImageResource(R.drawable.max_verstappen_number);
                 break;
 
             case 81:
                 // Oscar Piastri
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.mclaren_f1));
-                driverName.setText("Oscar Piastri");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.oscar_piastri);
+                driverNumber.setImageResource(R.drawable.oscar_piastri_number);
                 break;
 
             case 55:
                 // Carlos Sainz
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.ferrari_f1));
-                driverName.setText("Carlos Sainz");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.carlos_sainz);
+                driverNumber.setImageResource(R.drawable.carlos_sainz_number);
                 break;
 
             case 63:
                 // George Russell
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.mercedes_f1));
-                driverName.setText("George Russell");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.george_russell);
+                driverNumber.setImageResource(R.drawable.george_russell_number);
                 break;
 
             case 44:
                 // Lewis Hamilton
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.mercedes_f1));
-                driverName.setText("Lewis Hamilton");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.lewis_hamilton);
+                driverNumber.setImageResource(R.drawable.lewis_hamilton_number);
                 break;
 
             case 11:
                 // Sergio Perez
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.redbull_f1));
-                driverName.setText("Sergio Perez");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.sergio_perez);
+                driverNumber.setImageResource(R.drawable.sergio_perez_number);
                 break;
 
             case 14:
                 // Fernando Alonso
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.aston_martin_f1));
-                driverName.setText("Fernando Alonso");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.fernando_alonso);
+                driverNumber.setImageResource(R.drawable.fernando_alonso_number);
                 break;
 
             case 27:
                 // Nico Hulkenberg
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.haas_f1));
-                driverName.setText("Nico Hulkenberg");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.nico_hulkenberg);
+                driverNumber.setImageResource(R.drawable.nico_hulkenberg_number);
                 break;
 
             case 22:
                 // Yuki Tsunoda
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.racing_bulls_f1));
-                driverName.setText("Yuki Tsunoda");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.yuki_tsunoda);
+                driverNumber.setImageResource(R.drawable.yuki_tsunoda_number);
                 break;
 
             case 10:
                 // Pierre Gasly
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.alpine_f1));
-                driverName.setText("Pierre Gasly");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.pierre_gasly);
+                driverNumber.setImageResource(R.drawable.pierre_gasly_number);
                 break;
 
             case 18:
                 // Lance Stroll
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.aston_martin_f1));
-                driverName.setText("Lance Stroll");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.lance_stroll);
+                driverNumber.setImageResource(R.drawable.lance_stroll_number);
                 break;
 
             case 31:
                 // Esteban Ocon
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.alpine_f1));
-                driverName.setText("Esteban Ocon");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.esteban_ocon);
+                driverNumber.setImageResource(R.drawable.esteban_ocon_number);
                 break;
 
             case 20:
                 // Kevin Magnussen
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.haas_f1));
-                driverName.setText("Kevin Magnussen");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.kevin_magnussen);
+                driverNumber.setImageResource(R.drawable.kevin_magnussen_number);
                 break;
 
             case 23:
-                // Alex Albon
+                // Alexander Albon
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.williams_f1));
-                driverName.setText("Alex Albon");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.alexander_albon);
+                driverNumber.setImageResource(R.drawable.alexander_albon_number);
                 break;
 
             case 3:
                 // Daniel Ricciardo
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.racing_bulls_f1));
-                driverName.setText("Daniel Ricciardo");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.daniel_ricciardo);
+                driverNumber.setImageResource(R.drawable.daniel_ricciardo_number);
                 break;
 
             case 50:
                 // Oliver Bearmann
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.haas_f1));
-                driverName.setText("Oliver Bearmann");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.oliver_bearman);
+                driverNumber.setImageResource(R.drawable.oliver_bearman_number);
                 break;
 
             case 43:
                 // Franco Colapinto
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.williams_f1));
-                driverName.setText("Franco Colapinto");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.franco_colapinto);
+                driverNumber.setImageResource(R.drawable.franco_colapinto_number);
                 break;
 
             case 30:
                 // Liam Lawson
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.racing_bulls_f1));
-                driverName.setText("Liam Lawson");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.liam_lawson);
+                driverNumber.setImageResource(R.drawable.liam_lawson_number);
                 break;
 
             case 24:
                 // Zhou Guanyu
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.kick_f1));
-                driverName.setText("Zhou Guanyu");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.zhou_guanyu);
+                driverNumber.setImageResource(R.drawable.guanyu_zhou_number);
                 break;
 
             case 2:
                 // Logan Sargeant
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.williams_f1));
-                driverName.setText("Logan Sargeant");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.logan_sargeant);
+                driverNumber.setImageResource(R.drawable.logan_sargeant_number);
                 break;
 
             case 77:
                 // Valtteri Bottas
                 teamColor.setBackgroundColor(ContextCompat.getColor(this, R.color.kick_f1));
-                driverName.setText("Valtteri Bottas");
-                driverNumber.setImageResource(R.drawable.max_verstappen_number);
+                driverName.setText(R.string.valtteri_bottas);
+                driverNumber.setImageResource(R.drawable.valtteri_bottas_number);
                 break;
             default:
                 break;
