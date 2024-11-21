71b1d1f0cd65a637c2bfd52ec012a250e1e447f1
Lorenzo207378 <l.caputo13@campus.unimib.it>
2024-11-21 22:08:25 +0100

    minor fixes and adds
    fixed countdown layout;
    added onClickListener logic for all the interactive elements in the page

--- app/src/main/java/com/the_coffe_coders/fastestlap/EventActivity.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/EventActivity.java
@@ -10,11 +10,23 @@ import androidx.core.text.HtmlCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Layout;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.ViewGroup;
 
+import com.google.android.material.card.MaterialCardView;
+
+import org.w3c.dom.Text;
+
 public class EventActivity extends AppCompatActivity {
+
+    private static final String TAG = "EventActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,76 @@ public class EventActivity extends AppCompatActivity {
         ViewGroup.MarginLayoutParams layoutParams2 = (ViewGroup.MarginLayoutParams) textView2.getLayoutParams();
         layoutParams2.setMarginEnd(dynamicDimension2); // Set margin end to 20 pixels
         textView2.setLayoutParams(layoutParams2);
+
+       //create a countdown logic for countdown layout using days_counter, hours_counter, minutes_counter and seconds_counter
+        new CountDownTimer(1000000000, 1000) {
+            TextView days_counter = findViewById(R.id.days_counter);
+            TextView hours_counter = findViewById(R.id.hours_counter);
+            TextView minutes_counter = findViewById(R.id.minutes_counter);
+            TextView seconds_counter = findViewById(R.id.seconds_counter);
+
+            public void onTick(long millisUntilFinished) {
+                long days = millisUntilFinished / 86400000;
+                long hours = (millisUntilFinished % 86400000) / 3600000;
+                long minutes = ((millisUntilFinished % 86400000) % 3600000) / 60000;
+                long seconds = (((millisUntilFinished % 86400000) % 3600000) % 60000) / 1000;
+
+                days_counter.setText(String.valueOf(days));
+                hours_counter.setText(String.valueOf(hours));
+                minutes_counter.setText(String.valueOf(minutes));
+                seconds_counter.setText(String.valueOf(seconds));
+            }
+
+            public void onFinish() {
+                days_counter.setText("0");
+                hours_counter.setText("0");
+                minutes_counter.setText("0");
+                seconds_counter.setText("0");
+            }
+        }.start();
+
+        //add event listener logic for countdown layout
+        LinearLayout trackPic = findViewById(R.id.track_pic);
+        trackPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "clicked");
+
+            }
+        });
+
+        //add event listener logic for live_session card
+        MaterialCardView liveSession = findViewById(R.id.live_session);
+        liveSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "live session clicked");
+            }
+        });
+
+        //add event listener for weather card
+        MaterialCardView weatherCard = findViewById(R.id.weather_forecast);
+        weatherCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "weather card clicked");
+            }
+        });
+
+        //add event listener for session_1_flag
+        ImageView session1Flag = findViewById(R.id.session_1_flag);
+        session1Flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "session 1 flag clicked");
+            }
+        });
+
+
+
+
+
+
 
 
 
