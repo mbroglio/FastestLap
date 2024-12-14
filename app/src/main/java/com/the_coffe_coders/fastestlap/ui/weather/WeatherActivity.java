853e71288f695c54489d20d8728bd94a8c3e05cd
Lorenzo207378 <l.caputo13@campus.unimib.it>
2024-12-15 00:56:58 +0100

    minor fix
    added back navigation logic from weather page

--- app/src/main/java/com/the_coffe_coders/fastestlap/ui/weather/WeatherActivity.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/weather/WeatherActivity.java
@@ -2,11 +2,13 @@ package com.the_coffe_coders.fastestlap.ui.weather;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toolbar;
 import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.the_coffe_coders.fastestlap.R;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -16,6 +18,9 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
 
         VideoView videoView = findViewById(R.id.video_view);
         String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.sun_weather_video;
