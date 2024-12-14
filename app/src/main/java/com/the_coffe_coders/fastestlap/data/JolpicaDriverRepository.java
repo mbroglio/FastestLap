cd60d4d2642b8847a9ee6aceb6bd07a8fb1df0c5
fgargioli <f.gargioli@campus.unimib.it>
2024-12-14 11:24:34 +0100

    moved method
    getdrivers from server from JolpicaDriverRep to JolpicaDriverStandRep

--- app/src/main/java/com/the_coffe_coders/fastestlap/data/JolpicaDriverRepository.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/data/JolpicaDriverRepository.java
@@ -30,7 +30,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-public class JolpicaDriverRepository implements IDriverRepository, JolpicaServer{
+public class JolpicaDriverRepository implements IDriverRepository, JolpicaServer {
 
     String TAG = "RetrofitDriverRepository";
 
@@ -55,71 +55,6 @@ public class JolpicaDriverRepository implements IDriverRepository, JolpicaServer
         return null;
     }
 
-    public CompletableFuture<List<DriverDTO>> getDriversFromServer() {
-        CompletableFuture<List<DriverDTO>> future = new CompletableFuture<>();
-        String BASE_URL = "https://api.jolpi.ca/ergast/f1/" + 2024 + "/";
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .build();
-
-        ErgastAPI ergastApi = retrofit.create(ErgastAPI.class);
-
-        ergastApi.getDriverStandings().enqueue(new Callback<>() {
-            @Override
-            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    try {
-                        String responseString = response.body().string();
-                        JsonObject jsonResponse = new Gson().fromJson(responseString, JsonObject.class);
-                        JsonObject mrdata = jsonResponse.getAsJsonObject("MRData");
-
-                        JSONParserUtils jsonParserUtils = new JSONParserUtils();
-                        DriverStandingsAPIResponse driverStandingsAPIResponse = jsonParserUtils.parseDriverStandings(mrdata);
-
-                        List<DriverDTO> drivers = new ArrayList<>();
-                        int total = Integer.parseInt(driverStandingsAPIResponse.getTotal());
-                        for (int i = 0; i < total; i++) {
-                            DriverStandingsDTO standingElement = driverStandingsAPIResponse
-                                    .getStandingsTable()
-                                    .getStandingsLists().get(0)
-                                    .getDriverStandings().get(i);
-                            drivers.add(standingElement.getDriver());
-                        }
-
-                        future.complete(drivers); // Risolve il CompletableFuture
-                    } catch (Exception e) {
-                        //Log.e(TAG, "Failed to parse JSON response", e);
-                        future.completeExceptionally(e);
-                    }
-                } else {
-                    Log.e(TAG, "Response not successful");
-                    future.completeExceptionally(new Exception("Response not successful"));
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
-                Log.e(TAG, "Network request failed", t);
-                future.completeExceptionally(t);
-            }
-        });
-
-        return future;
-    }
-
-    public static void main(String[] args) {
-        CompletableFuture<List<DriverDTO>> future = new JolpicaDriverRepository().getDriversFromServer();
-
-        try {
-            List<DriverDTO> drivers = future.get();
-            for (DriverDTO driver : drivers) {
-                System.out.println(driver.toString());
-            }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
 
 
--- app/src/main/java/com/the_coffe_coders/fastestlap/data/JolpicaDriverStandingRepository.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/data/JolpicaDriverStandingRepository.java
@@ -1,5 +1,103 @@
 package com.the_coffe_coders.fastestlap.data;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.the_coffe_coders.fastestlap.api.DriverStandingsAPIResponse;
+import com.the_coffe_coders.fastestlap.api.ErgastAPI;
+import com.the_coffe_coders.fastestlap.dto.DriverDTO;
+import com.the_coffe_coders.fastestlap.dto.DriverStandingsDTO;
+import com.the_coffe_coders.fastestlap.utils.JSONParserUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
 public class JolpicaDriverStandingRepository {
+
+    String TAG = "JolpicaDriverStandingRepository";
+
+    public CompletableFuture<List<DriverDTO>> getDriversFromServer() {
+        CompletableFuture<List<DriverDTO>> future = new CompletableFuture<>();
+        String BASE_URL = "https://api.jolpi.ca/ergast/f1/" + 2024 + "/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+
+        ErgastAPI ergastApi = retrofit.create(ErgastAPI.class);
+
+        ergastApi.getDriverStandings().enqueue(new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    try {
+                        String responseString = response.body().string();
+                        JsonObject jsonResponse = new Gson().fromJson(responseString, JsonObject.class);
+                        JsonObject mrdata = jsonResponse.getAsJsonObject("MRData");
+
+                        JSONParserUtils jsonParserUtils = new JSONParserUtils();
+                        DriverStandingsAPIResponse driverStandingsAPIResponse = jsonParserUtils.parseDriverStandings(mrdata);
+
+                        List<DriverDTO> drivers = new ArrayList<>();
+                        int total = Integer.parseInt(driverStandingsAPIResponse.getTotal());
+                        for (int i = 0; i < total; i++) {
+                            DriverStandingsDTO standingElement = driverStandingsAPIResponse
+                                    .getStandingsTable()
+                                    .getStandingsLists().get(0)
+                                    .getDriverStandings().get(i);
+                            drivers.add(standingElement.getDriver());
+                        }
+
+                        future.complete(drivers); // Risolve il CompletableFuture
+                    } catch (Exception e) {
+                        //Log.e(TAG, "Failed to parse JSON response", e);
+                        future.completeExceptionally(e);
+                    }
+                } else {
+                    Log.e(TAG, "Response not successful");
+                    future.completeExceptionally(new Exception("Response not successful"));
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                Log.e(TAG, "Network request failed", t);
+                future.completeExceptionally(t);
+            }
+        });
+
+        return future;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        /*CompletableFuture<List<DriverDTO>> future = new JolpicaDriverRepository().getDriversFromServer();
+
+        try {
+            List<DriverDTO> drivers = future.get();
+            for (DriverDTO driver : drivers) {
+                System.out.println(driver.toString());
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+}
+
+
+
 
 }
