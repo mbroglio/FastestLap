43d68d5abbec5d38adf0b467ddf9f6e0baf02258
Gabriele Groppo <g.groppo@campus.unimib.it>
2024-12-22 15:19:00 +0100

    Auto stash before merge of "viewmodel_creation" and "mergedev-creationrepo"

--- app/src/main/java/com/the_coffe_coders/fastestlap/ui/bio/viewmodel/DriverViewModel.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/bio/viewmodel/DriverViewModel.java
@@ -2,6 +2,8 @@ package com.the_coffe_coders.fastestlap.ui.bio.viewmodel;
 
 import androidx.lifecycle.ViewModel;
 
+import com.the_coffe_coders.fastestlap.data.JolpicaDriverRepository;
+
 public class DriverViewModel extends ViewModel {
 
     private final JolpicaDriverRepository driverRepository;
