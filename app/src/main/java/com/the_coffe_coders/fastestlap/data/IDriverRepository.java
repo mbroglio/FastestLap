5167691b737496c6caab4d9f2491ec728085733b
Gabriele Groppo <g.groppo@campus.unimib.it>
2024-12-22 15:15:39 +0100

    Auto stash before merge of "viewmodel_creation" and "mergedev-creationrepo"

--- app/src/main/java/com/the_coffe_coders/fastestlap/data/IDriverRepository.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/data/IDriverRepository.java
@@ -5,7 +5,5 @@ import com.the_coffe_coders.fastestlap.domain.driver.Driver;
 import java.util.List;
 
 public interface IDriverRepository {
-    List<Driver> findDrivers();
-
-    Driver find(String id);
+    List<Driver> findAll();
 }
