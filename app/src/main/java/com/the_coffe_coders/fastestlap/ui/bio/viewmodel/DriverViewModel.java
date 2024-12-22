348b53ad778f4ebf0e90e7550ab4bfaae4a5756c
fgargioli <f.gargioli@campus.unimib.it>
2024-12-22 15:13:40 +0100

    created

--- app/src/main/java/com/the_coffe_coders/fastestlap/ui/bio/viewmodel/DriverViewModel.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/bio/viewmodel/DriverViewModel.java
@@ -1,4 +1,10 @@
 package com.the_coffe_coders.fastestlap.ui.bio.viewmodel;
 
-public class DriverViewModel {
+import androidx.lifecycle.ViewModel;
+
+public class DriverViewModel extends ViewModel {
+
+    private final JolpicaDriverRepository driverRepository;
+
+
 }
