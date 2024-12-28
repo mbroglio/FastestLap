e71fbaac6d4d67e539194145404efbf9017ce2de
Gabriele Groppo <g.groppo@campus.unimib.it>
2024-12-28 15:49:38 +0100

    RaceRepo

--- app/src/main/java/com/the_coffe_coders/fastestlap/repository/IRaceRepository.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/repository/IRaceRepository.java
@@ -1,12 +1,13 @@
 package com.the_coffe_coders.fastestlap.repository;
 
 import com.the_coffe_coders.fastestlap.domain.grand_prix.Race;
+import com.the_coffe_coders.fastestlap.domain.grand_prix.WeeklyRace;
 
 import java.util.List;
 
 public interface IRaceRepository {
-    List<Race> findAll();
-
-    Race find(String id);
+    List<WeeklyRace> fetchWeeklyRace(long lastUpdate);
+    WeeklyRace fetchNextRace(long lastUpdate);
+    WeeklyRace fetchLastRace(long lastUpdate);
 }
 
