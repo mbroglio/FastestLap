be0c2595f6d8642d8236afcfd3424edf570da92e
Lorenzo207378 <l.caputo13@campus.unimib.it>
2024-12-28 01:42:41 +0100

    interval card for portrait screen created

--- app/src/main/java/com/the_coffe_coders/fastestlap/ui/live/fragment/LiveEventFragment.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/live/fragment/LiveEventFragment.java
@@ -1,66 +1,45 @@
 package com.the_coffe_coders.fastestlap.ui.live.fragment;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import com.the_coffe_coders.fastestlap.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LiveEventFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class LiveEventFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public LiveEventFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LiveEventFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LiveEventFragment newInstance(String param1, String param2) {
-        LiveEventFragment fragment = new LiveEventFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_event, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_live_event, container, false);
+
+        CheckBox fullTelemetryCheckbox = view.findViewById(R.id.full_telemetry_checkbox);
+        fullTelemetryCheckbox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            } else {
+                requireActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+
+        return view;
     }
 }
