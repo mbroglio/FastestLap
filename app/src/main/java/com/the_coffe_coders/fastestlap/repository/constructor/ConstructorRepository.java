00ea260b5a08f1e60bff3e4d264e4f8a242b95a2
Gabriele Groppo <g.groppo@campus.unimib.it>
2024-12-28 14:08:48 +0100

    Removed unnecessary implementation of JolpicaServer Interface in ConstructorRepository

--- app/src/main/java/com/the_coffe_coders/fastestlap/repository/constructor/ConstructorRepository.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/repository/constructor/ConstructorRepository.java
@@ -43,7 +43,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-public class ConstructorRepository implements IConstructorRepository, JolpicaServer, ConstructorResponseCallback {
+public class ConstructorRepository implements IConstructorRepository, ConstructorResponseCallback {
 
     String TAG = "JolpicaConstructorRepository";
     public static long FRESH_TIMEOUT = 60000;
@@ -71,83 +71,11 @@ public class ConstructorRepository implements IConstructorRepository, JolpicaSer
 
     @Override
     public final List<Constructor> findConstructors() {
-     List<Constructor> constructors = new ArrayList<>();
-        try {
-            for (ConstructorDTO constructor: getConstructorsFromServer().get()) {
-                constructors.add(ConstructorMapper.toConstructor(constructor));
-            }
-        } catch (ExecutionException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return constructors;
-    }
-
-    public CompletableFuture<List<ConstructorDTO>> getConstructorsFromServer() {
-        CompletableFuture<List<ConstructorDTO>> future = new CompletableFuture<>();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(CURRENT_YEAR_BASE_URL)
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .build();
-
-        ErgastAPIService ergastApiService = retrofit.create(ErgastAPIService.class);
-
-        ergastApiService.getConstructorStandings().enqueue(new Callback<>() {
-            @Override
-            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    try {
-                        String responseString = response.body().string();
-                        JsonObject jsonResponse = new Gson().fromJson(responseString, JsonObject.class);
-                        JsonObject mrdata = jsonResponse.getAsJsonObject("MRData");
-
-                        JSONParserUtils jsonParserUtils = new JSONParserUtils();
-                        ConstructorStandingsAPIResponse constructorStandingsAPIResponse = jsonParserUtils.parseConstructorStandings(mrdata);
-
-                        List<ConstructorDTO> constructors = new ArrayList<>();
-                        int total = Integer.parseInt(constructorStandingsAPIResponse.getTotal());
-                        for (int i = 0; i < total; i++) {
-                            ConstructorStandingsElementDTO standingElement = constructorStandingsAPIResponse
-                                    .getStandingsTable()
-                                    .getStandingsLists().get(0)
-                                    .getConstructorStandings().get(i);
-                            constructors.add(standingElement.getConstructor());
-                        }
-
-                        future.complete(constructors); // Risolve il CompletableFuture
-                    } catch (Exception e) {
-                        //Log.e(TAG, "Failed to parse JSON response", e);
-                        future.completeExceptionally(e);
-                    }
-                } else {
-                    Log.e(TAG, "Response not successful");
-                    future.completeExceptionally(new Exception("Response not successful"));
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
-                Log.e(TAG, "Network request failed", t);
-                future.completeExceptionally(t);
-            }
-        });
-
-        return future;
+        return null;
     }
 
     @Override
     public Constructor find(String id) {
-
-        //TODO REMOVE
-        Year year = Year.now();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL + year + "/")
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .build();
-
-        ErgastAPIService ergastApiService = retrofit.create(ErgastAPIService.class);
-
         return null;
     }
 
