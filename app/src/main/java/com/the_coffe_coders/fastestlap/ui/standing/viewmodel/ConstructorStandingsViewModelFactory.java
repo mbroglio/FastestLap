5d5580ec6ddac51f689fb174b29bc6bce137ef77
fgargioli <f.gargioli@campus.unimib.it>
2024-12-30 22:25:36 +0100

    Completed ConstrucotrStandingsViewModelFactory

--- app/src/main/java/com/the_coffe_coders/fastestlap/ui/standing/viewmodel/ConstructorStandingsViewModelFactory.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/standing/viewmodel/ConstructorStandingsViewModelFactory.java
@@ -1,8 +1,29 @@
 package com.the_coffe_coders.fastestlap.ui.standing.viewmodel;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.the_coffe_coders.fastestlap.repository.constructor.ConstructorRepository;
+import com.the_coffe_coders.fastestlap.repository.driver.DriverRepository;
+
 public class ConstructorStandingsViewModelFactory implements ViewModelProvider.Factory {
-    
+    private final ConstructorRepository constructorRepository;
+
+    public ConstructorStandingsViewModelFactory(ConstructorRepository constructorRepository)  {
+        this.constructorRepository = constructorRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(DriverStandingsViewModel.class)) {
+            // Suppress unchecked cast warning
+            @SuppressWarnings("unchecked")
+            T viewModel = (T) new ConstructorStandingsViewModel(constructorRepository);
+            return viewModel;
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
 }
