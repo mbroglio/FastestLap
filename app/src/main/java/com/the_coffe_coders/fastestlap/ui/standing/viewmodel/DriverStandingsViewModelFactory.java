bec70b9c45e4d802464e472fcae7efc8b2cf6b3c
Gabriele Groppo <g.groppo@campus.unimib.it>
2024-12-30 22:17:58 +0100

    feat(app): Added DriverStandingsViewModelFactory
    Changelog: feature

--- app/src/main/java/com/the_coffe_coders/fastestlap/ui/standing/viewmodel/DriverStandingsViewModelFactory.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/standing/viewmodel/DriverStandingsViewModelFactory.java
@@ -1,6 +1,28 @@
 package com.the_coffe_coders.fastestlap.ui.standing.viewmodel;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.the_coffe_coders.fastestlap.repository.driver.DriverRepository;
+
 public class DriverStandingsViewModelFactory implements ViewModelProvider.Factory {
+    private final DriverRepository driverRepository;
+
+    public DriverStandingsViewModelFactory(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(DriverStandingsViewModel.class)) {
+            // Suppress unchecked cast warning
+            @SuppressWarnings("unchecked")
+            T viewModel = (T) new DriverStandingsViewModel(driverRepository);
+            return viewModel;
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
 }
