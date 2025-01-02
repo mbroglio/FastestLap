e0989d5fe4b85b323c231540b2b13d0c0189457a
Lorenzo207378 <l.caputo13@campus.unimib.it>
2025-01-02 22:00:40 +0100

    1/2 commit: importing login pages and layouts

---
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/login/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.the_coffe_coders.fastestlap.ui.login;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.the_coffe_coders.fastestlap.R;
+import com.the_coffe_coders.fastestlap.util.IntroScreen;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginFragment";
+    private IntroScreen introScreen;
+    private TextInputEditText emailEditText;
+    private TextInputEditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        introScreen = new IntroScreen(findViewById(android.R.id.content), this);
+        introScreen.showIntroScreen();
+
+        emailEditText = findViewById(R.id.textInputEmail);
+        passwordEditText = findViewById(R.id.textInputPassword);
+
+        View rootView = findViewById(R.id.login_activity_layout);
+        rootView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetHintPosition();
+            }
+        });
+
+        Button registerButton = findViewById(R.id.RegisterButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRegisterPopup();
+            }
+        });
+    }
+
+    private void showRegisterPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.register_pop_up, null);
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
+
+        Button submitButton = dialogView.findViewById(R.id.submit_button);
+        EditText emailEditText = dialogView.findViewById(R.id.textInputEmail);
+        EditText passwordEditText = dialogView.findViewById(R.id.textInputPassword);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                Log.d(TAG, "Email: " + email);
+                Log.d(TAG, "Password: " + password);
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (introScreen != null) {
+            introScreen.releaseResources();
+        }
+    }
+
+    private void resetHintPosition() {
+        emailEditText.clearFocus();
+        passwordEditText.clearFocus();
+    }
+}
