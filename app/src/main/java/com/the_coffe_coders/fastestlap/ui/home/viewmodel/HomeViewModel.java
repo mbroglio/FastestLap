f291817d639971e00d13e6ba0e7609d7201b652d
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-01-24 19:04:55 +0100

    some fixes

--- app/src/main/java/com/the_coffe_coders/fastestlap/ui/home/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/home/viewmodel/HomeViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.the_coffe_coders.fastestlap.domain.Result;
+import com.the_coffe_coders.fastestlap.domain.grand_prix.DriverStandingsElement;
 import com.the_coffe_coders.fastestlap.repository.constructor.ConstructorRepository;
 import com.the_coffe_coders.fastestlap.repository.driver.DriverRepository;
 import com.the_coffe_coders.fastestlap.repository.result.RaceResultRepository;
@@ -12,7 +13,10 @@ import com.the_coffe_coders.fastestlap.repository.weeklyrace.RaceRepository;
 import com.the_coffe_coders.fastestlap.ui.event.viewmodel.EventViewModel;
 import com.the_coffe_coders.fastestlap.ui.standing.viewmodel.DriverStandingsViewModel;
 import com.the_coffe_coders.fastestlap.ui.standing.viewmodel.DriverStandingsViewModelFactory;
+import com.the_coffe_coders.fastestlap.util.Constants;
 import com.the_coffe_coders.fastestlap.util.ServiceLocator;
+
+import java.util.List;
 
 public class HomeViewModel extends ViewModel {
     private static final String TAG = EventViewModel.class.getSimpleName();
@@ -53,6 +57,16 @@ public class HomeViewModel extends ViewModel {
         }
         return driverStandings;
     }
+    public DriverStandingsElement getDriverStandingsElement(List<DriverStandingsElement> driversList, String driverID) {
+        for (DriverStandingsElement driver : driversList) {
+            if (driver.getDriver().getDriverId().equals(driverID)) {
+                return driver;
+            }
+        }
+
+        return null;
+    }
+
 
 
 
