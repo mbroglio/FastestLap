3c3a21b61e3fc526322e2219d689838ffbeec753
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-03-24 22:29:58 +0100

    Fix default value for year 2025 not current anymore due to jolpica issue
    Removed the last db

--- app/src/main/java/com/the_coffe_coders/fastestlap/database/RaceResultDAO.java
+++ app/src/main/java/com/the_coffe_coders/fastestlap/database/RaceResultDAO.java
@@ -2,6 +2,7 @@ package com.the_coffe_coders.fastestlap.database;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import com.the_coffe_coders.fastestlap.domain.grand_prix.RaceResult;
@@ -10,7 +11,7 @@ import java.util.List;
 
 @Dao
 public interface RaceResultDAO {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(RaceResult raceResult);
 
     @Query("SELECT * FROM RaceResult WHERE uid = :id")
