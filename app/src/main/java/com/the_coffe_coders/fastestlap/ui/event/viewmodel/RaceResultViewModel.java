beb8cdcc80e488afa7cd57e59629d0f885d72444
Matty-Tuber <m.broglio3@campus.unimib.it>
2025-05-22 22:04:26 +0200

    Started Implementing RaceResult Local DB: Introduced RaceResultViewModel and Factory
    This commit introduces `RaceResultViewModel` and its corresponding factory, `RaceResultViewModelFactory`.
    
    - `RaceResultViewModel` is responsible for providing race result data to the UI. It interacts with `ResultRepository` to fetch all race results or results for a specific race.
    - `RaceResultViewModelFactory` is responsible for creating instances of `RaceResultViewModel`, injecting the `ResultRepository` dependency.

---
+++ app/src/main/java/com/the_coffe_coders/fastestlap/ui/event/viewmodel/RaceResultViewModel.java
@@ -0,0 +1,24 @@
+package com.the_coffe_coders.fastestlap.ui.event.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.the_coffe_coders.fastestlap.domain.Result;
+import com.the_coffe_coders.fastestlap.repository.result.ResultRepository;
+
+public class RaceResultViewModel extends ViewModel {
+
+    private final ResultRepository resultRepository;
+
+    public RaceResultViewModel(ResultRepository resultRepository) {
+        this.resultRepository = resultRepository;
+    }
+
+    public MutableLiveData<Result> getAllRaceResults(int numberOfRaces) {
+        return resultRepository.fetchAllRaceResults(numberOfRaces);
+    }
+
+    public MutableLiveData<Result> getRaceResults(String raceId) {
+        return resultRepository.fetchResults(raceId);
+    }
+}
