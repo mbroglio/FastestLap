42da2d00bb0e4511d587196f79bff4b05a30ab48
mbroglio <m.broglio3@campus.unimib.it>
2026-07-17 18:03:49 +0200

    Refactor: Update compileSdk version to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.the_coffe_coders.fastestlap"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.the_coffe_coders.fastestlap"
