82ace2464db54024b9bec9da6231ce493dab0446
mbroglio <m.broglio3@campus.unimib.it>
2026-07-21 19:55:27 +0200

    Fixed Warnings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.the_coffe_coders.fastestlap"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
